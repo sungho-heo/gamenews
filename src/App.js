b1e05b503a5e3612e2cb9f173582f8329d594ac2
Sungho <hurgj123kr@gmail.com>
2023-09-28 19:31:47 +0900

    first game genshin data get

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=genshin&apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+      console.log(response.data);
+    };
+    getData();
+  }, []);
   return (
     <div className="App">
       <h2>Hello</h2>
