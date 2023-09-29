ef8ea3b7772f47c5d0aabbb65660b62352cf5a3d
Sungho <hurgj123kr@gmail.com>
2023-09-29 16:04:44 +0900

    popularty 10 news data get

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [gameNews, setGameNews] = useState([]);
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get(
-        `https://newsapi.org/v2/everything?q=genshin&apiKey=${process.env.REACT_APP_API_KEY}`
+        `${process.env.REACT_APP_API_URL}v2/everything?q=game&pageSize=10&language=en&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`
       );
-      console.log(response.data);
+      const data = response.data;
+      const gameArray = data.articles;
+      setGameNews(gameArray);
     };
     getData();
-  }, []);
+  }, [gameNews]);
   return (
     <div className="App">
       <h2>Hello</h2>
+      {gameNews.length > 0 ? (
+        gameNews.map((news, index) => (
+          <div key={index}>
+            <h3>{news.title}</h3>
+            <span>{news.author}</span>
+            <span>{news.description}</span>
+          </div>
+        ))
+      ) : (
+        <h3>Loading...</h3>
+      )}
     </div>
   );
 }
