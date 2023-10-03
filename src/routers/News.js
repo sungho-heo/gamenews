fff395e5fccc649ae09125b7838bd7e76d6015d1
Sungho <hurgj123kr@gmail.com>
2023-10-03 18:07:04 +0900

    news css update

--- src/routers/News.js
+++ src/routers/News.js
@@ -1,17 +1,18 @@
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import style from "../css/News.module.css";
 
 export default function News({ gameNews }) {
   const { id } = useParams();
   return (
     <div>
-      <h1>{gameNews[id].title}</h1>
+      <h1 className={style.title}>{gameNews[id].title}</h1>
       <div>
-        <div>
+        <div className={style.main}>
           <img
             src={gameNews[id].urlToImage}
             alt={gameNews[id].title}
-            className={"img"}
+            className={style.detailImage}
           />
           <p>
             {new Date(gameNews[id].publishedAt).toLocaleString("en-us", {
